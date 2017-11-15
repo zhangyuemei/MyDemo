69e9d61694fba566c6038ebbfa203f59b15a6e2d
zhangyuemei <zhangyuemei_yx2@163.com>
2017-11-15 18:01:59 +0800

    init

--- app/src/main/java/com/zhangyuemei/mydemo/MainActivity.java
+++ app/src/main/java/com/zhangyuemei/mydemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.zhangyuemei.mydemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.e("111","");
     }
 }
