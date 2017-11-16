02ee74ac81fa80d76f03e67d43cd22591e2308cf
zhangyuemei <zhangyuemei_yx2@163.com>
2017-11-16 10:23:40 +0800

    修改

---
+++ app/src/main/java/com/zhangyuemei/mydemo/Base.java
@@ -0,0 +1,11 @@
+package com.zhangyuemei.mydemo;
+
+/**
+ * created by zhangyuemei on  2017/11/16.
+ * Email :  2501985075@qq.com
+ * explain :
+ */
+
+public class Base {
+
+}
---
+++ app/src/main/java/com/zhangyuemei/mydemo/Test.java
@@ -0,0 +1,12 @@
+package com.zhangyuemei.mydemo;
+
+/**
+ * @author : zhangyuemei
+ *         CreateTime : 2017/11/16.
+ *         Email :  2501985075@qq.com
+ *         explain :
+ */
+
+public class Test {
+    
+}
